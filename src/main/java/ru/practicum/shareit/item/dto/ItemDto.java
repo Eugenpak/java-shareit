60df3c421a027bfa4f1de2c7ed67461a64c9c712
Eugenpak <techteamit@yandex.ru>
2025-04-26 18:28:11 +0300

    Сохранить изменения в пакете item.dto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
 }
