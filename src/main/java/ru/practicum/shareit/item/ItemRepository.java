c7194b4fd0e7e6f15a4e764e381abb1b355c8a01
Eugenpak <techteamit@yandex.ru>
2025-04-26 18:32:30 +0300

    Сохранить изменения в пакете item: ItemRepository, ItemRepositoryInMemory

---
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository {
+    boolean delItemById(long itemId);
+    Optional<Item> findItemById(long itemId);
+    Item update(Item item);
+    Item create(Item item);
+    List<Item> findAll(long userId);
+    List<Item> getBySearch(String text);
+}
