88627c54bdc036fb095fe22260c5cc55a0f14e60
Eugenpak <techteamit@yandex.ru>
2025-04-26 18:30:32 +0300

    Сохранить изменения в пакете item: ItemController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,68 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
+@Slf4j
 public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping
+    public List<ItemDto> get(@NotNull @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("-----------------------------------------|get|------");
+        log.info("I-C -> get(X-Sharer-User-Id: {})",userId);
+        return itemService.getItems(userId);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getById(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
+                           @PathVariable(name="itemId") long itemId) {
+        log.info("-----------------------------------------|getById|------");
+        log.info("I-C -> getById(X-Sharer-User-Id: {},itemId: {})",userId,itemId);
+        return itemService.getByItemId(userId,itemId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> getBySearch(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
+                                 @RequestParam(name = "text", required = false) String text) {
+        log.info("-----------------------------------------|getBySearch|------");
+        log.info("I-C -> getBySearch(X-Sharer-User-Id: {},text: {})",userId,text);
+        return itemService.getBySearch(userId,text);
+    }
+
+    @PostMapping
+    public ItemDto add(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
+                       @RequestBody ItemDto itemDto) {
+        log.info("-----------------------------------------|add|------");
+        log.info("I-C -> add(X-Sharer-User-Id: {},itemDto: {})",userId,itemDto);
+        return itemService.addNewItem(userId, itemDto);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto update(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
+                          @PathVariable(name="itemId") long itemId,
+                          @RequestBody ItemDto itemDto) {
+        log.info("-----------------------------------------|update|------");
+        log.info("I-C -> update(X-Sharer-User-Id: {},itemId: {},itemDto: {})",userId,itemId,itemDto);
+        return itemService.updateItem(userId, itemId,itemDto);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void deleteItem(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
+                           @PathVariable(name="itemId") long itemId) {
+        log.info("-----------------------------------------|deleteItem|------");
+        log.info("I-C -> deleteItem(X-Sharer-User-Id: {},itemId: {})",userId,itemId);
+        itemService.deleteItem(userId, itemId);
+    }
 }
