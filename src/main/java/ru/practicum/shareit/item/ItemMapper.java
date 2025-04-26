1154bdc67259b9b87ad0297043c13fabd9c1511b
Eugenpak <techteamit@yandex.ru>
2025-04-26 18:31:54 +0300

    Сохранить изменения в пакете item: ItemMapper

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,38 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ItemMapper {
+    public ItemDto toDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
+    }
+
+    public Item fromDto(ItemDto itemDto,Long userId) {
+        return Item.builder()
+                .id(itemDto.getId())
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .ownerId(userId)
+                .build();
+    }
+
+    public List<ItemDto> toDtos(List<Item> items) {
+        List<ItemDto> result = new ArrayList<>();
+        for (Item item : items) {
+            result.add(toDto(item));
+        }
+        return result;
+    }
+}
