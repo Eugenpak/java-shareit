7bc9188ea4bbfd98c3040cb74d440cf85f1f6a07
Eugenpak <techteamit@yandex.ru>
2025-06-01 09:02:43 +0300

    Добавить тесты для пакета exception.

---
+++ src/test/java/ru/practicum/shareit/exception/RestExceptionHandlerTest.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.exception;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+class RestExceptionHandlerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenMethodArgumentNotValid_thenBadRequest() throws Exception {
+        mockMvc.perform(post("/endpoint") .content("{...}") .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Ошибка валидации данных"));
+    }
+}
