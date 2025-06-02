b4b018e2d39da19c89a6da1c7b538859f24bfe68
Eugenpak <techteamit@yandex.ru>
2025-06-03 00:36:37 +0300

    Сохранить пакет request в модуле gateway.

---
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.request.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.validation.group.Create;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Value
+public class RequestDto {
+    Long id;
+    @NotBlank(groups = {Create.class})
+    String description;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
+    LocalDateTime created;
+    List<ItemDto> items;
+}
